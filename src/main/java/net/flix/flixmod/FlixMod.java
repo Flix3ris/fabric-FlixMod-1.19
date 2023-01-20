f4e157b31b23919d838833d2b0ad51fa34cfaac5
Flix3ris <fepa899@gmail.com>
2023-01-20 19:13:13 +0200

    Initial commit

---
+++ src/main/java/net/flix/flixmod/FlixMod.java
@@ -0,0 +1,14 @@
+package net.flix.flixmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlixMod implements ModInitializer {
+	public static final String MOD_ID = "flixmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/flix/flixmod/FlixModClient.java
@@ -0,0 +1,10 @@
+package net.flix.flixmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlixModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
